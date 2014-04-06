818af00c605c86d6a3252654bc8ba0600a24638f
sunghwano <sunghwano@NOH>
2014-04-06 16:05:05 +0900

    .

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,24 @@
 $(function(){
+	
 	$("#btnTopmain").click(function(){
 		$("#contentBox").css({
 			"display": ""
+		});
+		
+		$("#content1").css({
+			"display": "none"
+		});
+	});
+	
+	
+	
+	$("#btnNext").click(function(){
+		$("#content1").css({
+			"display": ""
+		});
+		
+		$("#contentBox").css({
+			"display": "none"
 		});
 	});
 });
